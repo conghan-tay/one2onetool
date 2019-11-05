d6f104d5e65c7b471ee06b86f38594a56ea700fd
conghan-tay <cohanitay@gmail.com>
2019-11-05 20:52:18 +0800

    Add detached

--- checkRunning.sh
+++ checkRunning.sh
@@ -4,4 +4,4 @@ if [ '$(docker ps -a -q -f name=current_staging)' ]; then
 fi
 
 echo "Running New Container"
-docker run -p 4000:4000 --name current_staging $registry:$BUILD_NUMBER
+docker run -d -p 4000:4000 --name current_staging $registry:$BUILD_NUMBER
