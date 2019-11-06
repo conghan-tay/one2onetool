df96608083498e597592b39035d5cd86cef48cea
conghan-tay <cohanitay@gmail.com>
2019-11-06 16:52:38 +0800

    remove old container

--- checkRunning.sh
+++ checkRunning.sh
@@ -3,7 +3,7 @@
 set -o xtrace
 ssh root@157.245.124.180
 if [ '$(docker ps -a -q -f name=current_release)' ]; then
-    docker rm current_release
+    docker rm -vf current_release
     echo "Removing Old Containers"
 fi
 
